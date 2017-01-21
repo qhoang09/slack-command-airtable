ba6c1714054807ff969a216ac0568e97deab8a18
Quang Hoang <qhoang09@gmail.com>
2017-01-21 18:00:37 +0100

    initial code on Airtable

--- app/controllers/airtable_controller.rb
+++ app/controllers/airtable_controller.rb
@@ -1,5 +1,8 @@
 class AirtableController < ApplicationController
   def index
-  	
+  		@client = Airtable::Client.new("keysLF5bncTFH50wm")
+		@table = @client.table("app3JvjS2LKzXALwY", "Helpees")
+		@records = @table.records
+		@first_record = @records.first
   end
 end
