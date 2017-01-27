03eff79756a9b27ef8857b8408c747e4c113fdcb
Quang Hoang <qhoang09@gmail.com>
2017-01-27 19:05:03 +0100

    test hello

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -1,8 +1,8 @@
 class CommandsController < ApplicationController
 	def create
-    return render json: {}, status: 403 unless valid_slack_token?
-    CommandWorker.perform_async(command_params.to_h)
-    render json: { response_type: "in_channel" }, status: :created
+#    return render json: {}, status: 403 unless valid_slack_token?
+ #   CommandWorker.perform_async(command_params.to_h)
+  	render json: "hello", status: :ok
   end
 
   private
