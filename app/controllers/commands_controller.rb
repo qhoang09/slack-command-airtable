226f35c3e3cb60197f26f9db7511f0910adb5193
Quang Hoang <qhoang09@gmail.com>
2017-01-27 19:33:49 +0100

    case commands

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -1,9 +1,21 @@
 class CommandsController < ApplicationController
 	protect_from_forgery
-	
-	def create
 
-  	render json: "hello", status: :ok
+	def create
+		command = params.fetch('command')
+		command_parameters = params.fetch('text')
+		case command
+		when '/testhello'
+			render json:'hello', status: :ok
+		when '/helpeecount'
+
+
+			@client = Airtable::Client.new("keysLF5bncTFH50wm")
+		
+			helpees_table = @client.table("app3JvjS2LKzXALwY", "Helpees")
+			render text: "There are #{helpees_table.records.count} helpees", status: :ok
+		end
+
   end
   
 end
