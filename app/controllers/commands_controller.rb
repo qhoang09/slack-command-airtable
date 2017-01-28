5628690d10b48c0438d57ddfb9336f549519813c
Quang Hoang <qhoang09@gmail.com>
2017-01-28 20:46:38 +0100

    field name

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -28,7 +28,7 @@ class CommandsController < ApplicationController
 			      ],
 			      "pretext": "Here is some information about #{current_helpor.name}",
 			      "attachment_type": "default",
-			      "author_subname": "#{current_helpor.Helpor_company}",
+			      "author_subname": "#{current_helpor.HelporCompany}",
 			      "title": "Bio",
 			      "fields": [
 			        {
